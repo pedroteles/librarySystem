b1cc1cc2189b67c033d2202f46bf591c4b8f60ca
pedroteles <pedrofelipegt@gmail.com>
2015-03-30 18:10:04 +0100

    SQL

---
+++ SQL/book instance details.sql
@@ -0,0 +1,11 @@
+﻿SELECT [b].[BookId], [Isbn], [Title], [Edition], [Summary], [BookInstanceId], [CallNumber],
+CASE WHEN (SELECT ReturnDate FROM Loans AS [l] WHERE [l].[BookInstanceId]=[bi].[BookInstanceId] AND [l].[LoanId] IN 
+(SELECT MAX(LoanId) FROM  Loans AS [l2] WHERE [l2].[BookInstanceId] = [bi].[BookInstanceId] ) ) 
+IS NULL AND 
+EXISTS (SELECT [l].[Date] FROM Loans AS [l] WHERE [l].[BookInstanceId]=[bi].[BookInstanceId])
+THEN 'CHECKED-OUT' ELSE 'Available' END AS [Status]
+FROM Books AS b 
+INNER JOIN 
+BooksInstances AS bi
+ON [b].[BookId]=[bi].[BookId]
+WHERE [b].[BookId] = 1; 
