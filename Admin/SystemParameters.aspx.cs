ba1c2e3b7c85187def0a236dd67556d2ab0f2e59
pedroteles <pedrofelipegt@gmail.com>
2015-04-02 16:12:52 +0100

    Select on page load - text boxes with data from DB

--- Admin/SystemParameters.aspx.cs
+++ Admin/SystemParameters.aspx.cs
@@ -11,6 +11,26 @@ public partial class Admin_SystemParameters : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+       
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\aspnet-librarySystem-20150310153417.mdf;Integrated Security=True");
+        con.Open();
+
+        using (SqlCommand cmd = new SqlCommand("SELECT [Fine], [MaximumRenewals], [MaximumItens], [NumberOfDays] FROM SystemParameters", con)) 
+        {
+        SqlDataReader dr = cmd.ExecuteReader();
+
+        while (dr.Read())
+        {
+            txtFine.Text = dr["Fine"].ToString();
+            txtMaxR.Text = dr["MaximumRenewals"].ToString();
+            txtMaxIt.Text = dr["MaximumItens"].ToString();
+            txtNum.Text = dr["NumberOfDays"].ToString();
+
+        }
+
+        dr.Close();
+        }
+
 
     }
     protected void btnsubmit_Click(object sender, EventArgs e)
