644da2e404bd36de77ed95ca77736ad90d3dbd80
pedroteles <pedrofelipegt@gmail.com>
2015-04-06 04:04:22 +0100

    SQL change (Collections.aspx); Insert and edit books (ManageBooks)

--- Librarian/ManageBooks.aspx
+++ Librarian/ManageBooks.aspx
@@ -17,21 +17,35 @@
            <asp:Menu ID="BooksMenu" Width="168px" runat="server" Orientation="Horizontal" StaticEnableDefaultPopOutImage="False" OnMenuItemClick="BooksMenu_MenuItemClick">
                 <Items>
                     <asp:MenuItem ImageUrl="../Images/orderedList0.png" 
-                                  Text="New Book" Value="2"></asp:MenuItem>
+                                  Text="New Book" Value="1"></asp:MenuItem>
                 </Items>
            </asp:Menu>
            <asp:MultiView ID="BooksView" runat="server" ActiveViewIndex="0"  >
                <asp:View ID="BooksTable" runat="server">
-                   <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:LibraryDatabase %>" SelectCommand="SELECT * FROM [Books]" EnableCaching="false"></asp:SqlDataSource>
-                   <asp:GridView ID="GridView1" runat="server" AllowPaging="True" AllowSorting="True" AutoGenerateColumns="False" DataKeyNames="BookId" DataSourceID="SqlDataSource1" CellPadding="4" ForeColor="#333333" GridLines="None" OnSelectedIndexChanging="GridView1_SelectedIndexChanging">
+                   <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:LibraryDatabase %>" SelectCommand="SELECT [b].[BookId], [Isbn], [Title], [Edition], [Summary],	 Subjects = STUFF(
+             (SELECT '; ' + SubjectName
+              FROM Subjects s INNER JOIN SubjectsInBooks sb on s.SubjectId = sb.SubjectId 
+			  WHERE sb.BookId=b.BookId
+              FOR XML PATH (''))
+             , 1, 1, ''), 
+        Authors = STUFF(
+             (SELECT '; ' + AuthorSurname +', '+ AuthorFirstName
+              FROM Authors a INNER JOIN AuthorsInBooks ab on a.AuthorId = ab.AuthorId 
+			  WHERE ab.BookId=b.BookId
+              FOR XML PATH (''))
+             , 1, 1, '') 
+FROM Books AS [b]" EnableCaching="false"></asp:SqlDataSource>
+                   <asp:GridView ID="GridView1" runat="server" AllowPaging="True" AllowSorting="True" AutoGenerateColumns="False" DataKeyNames="BookId" DataSourceID="SqlDataSource1" CellPadding="4" ForeColor="#333333" GridLines="None" OnSelectedIndexChanging="editBook">
                        <AlternatingRowStyle BackColor="White" />
                        <Columns>
                            <asp:CommandField ShowSelectButton="True" SelectText="Edit"/>
                            <asp:BoundField DataField="BookId" HeaderText="BookId" InsertVisible="False" ReadOnly="True" SortExpression="BookId" />
                            <asp:BoundField DataField="Isbn" HeaderText="Isbn" SortExpression="Isbn" />
                            <asp:BoundField DataField="Title" HeaderText="Title" SortExpression="Title" />
-                           <asp:BoundField DataField="Summary" HeaderText="Summary" SortExpression="Summary" />
                            <asp:BoundField DataField="Edition" HeaderText="Edition" SortExpression="Edition" />
+                           <asp:BoundField DataField="Summary" HeaderText="Summary" SortExpression="Summary" />
+                           <asp:BoundField DataField="Subjects" HeaderText="Subjects" ReadOnly="True" SortExpression="Subjects" />
+                           <asp:BoundField DataField="Authors" HeaderText="Authors" ReadOnly="True" SortExpression="Authors" />
                        </Columns>
                        <EditRowStyle BackColor="#2461BF" />
                        <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
@@ -45,11 +59,9 @@
                        <SortedDescendingHeaderStyle BackColor="#4870BE" />
                    </asp:GridView>
                    </asp:View>
-               <asp:View ID="EditBook" runat="server"  >                  
-                   <asp:Button ID="btnEditSave" runat="server" Text="Save" OnClick="btnEditSave_Click" />
-               </asp:View>
-               <asp:View ID="NewBook" runat="server"  >
-                   <asp:Wizard ID="Wizard1" runat="server" ActiveStepIndex="0" OnFinishButtonClick="Wizard1_FinishButtonClick" DisplaySideBar="False" BackColor="#F7F6F3" BorderColor="#CCCCCC" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="0.8em" >
+               
+               <asp:View ID="BookDetails" runat="server"  >
+                   <asp:Wizard ID="Wizard1" runat="server" ActiveStepIndex="0" OnFinishButtonClick="Wizard1_FinishButtonClick" DisplaySideBar="False" BackColor="#F7F6F3" BorderColor="#CCCCCC" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="0.8em" OnNextButtonClick="Wizard1_NextButtonClick">
                        <FinishNavigationTemplate>
                            <asp:Button ID="FinishPreviousButton" runat="server" CausesValidation="False" CommandName="MovePrevious" Text="Previous" />
                            <asp:Button ID="FinishButton" runat="server" CommandName="MoveComplete" Text="Finish" />
@@ -102,16 +114,39 @@
                                </asp:Table>
                            </asp:WizardStep>
                            <asp:WizardStep runat="server" title="Authors">
-                               <asp:ListBox ID="ListBox1" runat="server">
-                                   <asp:ListItem>Author1</asp:ListItem>
-                                   <asp:ListItem>Author2</asp:ListItem>
+                                                         
+
+                                   
+                               <asp:SqlDataSource ID="SqlDataSource3" runat="server" ConnectionString="<%$ ConnectionStrings:LibraryDatabase %>" SelectCommand="SELECT [AuthorId], AuthorFirstName+' ' +AuthorSurname AS [AuthorName] FROM [Authors]"></asp:SqlDataSource>
+                               <asp:Label ID="Label1" runat="server" Text="All Authors"></asp:Label><br />
+                               <asp:ListBox ID="lstNewAllAuthors" runat="server" DataSourceID="SqlDataSource3" DataTextField="AuthorName" DataValueField="AuthorId"></asp:ListBox>
+                               <asp:Button ID="btnNewAuthorAdd" runat="server" Text="Add" OnClick="btnNewAuthorAdd_Click"/>
+                               
+                               <br />
+                                   
+                               <asp:Label ID="lblNewBookAuthors" runat="server" Text="Book Authors"></asp:Label><br />
+                                   
+                               <asp:ListBox ID="lstNewAuthors" runat="server">
                                </asp:ListBox>
+                              
+                               
+                               
+                              
+                               <asp:Button ID="btnRemoveNewAuthor" runat="server" OnClick="btnRemoveNewAuthor_Click" Text="Remove" />
+                              
+                               
+                               
+                              
                            </asp:WizardStep>
                            <asp:WizardStep runat="server" Title="Subjects">
-                               <asp:ListBox ID="ListBox2" runat="server">
-                                   <asp:ListItem>Subject 1</asp:ListItem>
-                                   <asp:ListItem>Subject 2</asp:ListItem>
-                               </asp:ListBox>
+                               <asp:SqlDataSource ID="SqlDataSourceSubjects" runat="server" ConnectionString="<%$ ConnectionStrings:LibraryDatabase %>" SelectCommand="SELECT * FROM [Subjects]"></asp:SqlDataSource>
+                               <asp:Label ID="lblAllNewSubjects" runat="server" Text="All Subjects"></asp:Label><br />
+                               <asp:ListBox ID="lstNewAllSubjects" runat="server" DataSourceID="SqlDataSourceSubjects" DataTextField="SubjectName" DataValueField="SubjectId"></asp:ListBox>
+                               <asp:Button ID="btnAddNewSubject" runat="server" Text="Add" OnClick="btnAddNewSubject_Click" /><br />
+                               <asp:Label ID="lblNewSubjects" runat="server" Text="Book Subjects"></asp:Label><br />
+                               <asp:ListBox ID="lstNewSubjects" runat="server"></asp:ListBox>
+                               <asp:Button ID="btnRemoveNewSubject" runat="server" OnClick="btnRemoveNewSubject_Click" Text="Remove" />
+                               <br />
                            </asp:WizardStep>
                        </WizardSteps>
                    </asp:Wizard>
--- Librarian/ManageBooks.aspx.cs
+++ Librarian/ManageBooks.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -9,6 +10,36 @@ using System.Web.UI.WebControls;
 
 public partial class Librarian_ManageBooks : System.Web.UI.Page
 {
+
+
+
+    private Hashtable tableAuthors
+    {
+
+        get
+        {
+
+            return ViewState["tableAuthors"] as Hashtable;
+        }
+        set
+        {
+            ViewState["tableAuthors"] = value;
+        }
+    }
+
+    private Hashtable tableSubjects
+    {
+        get
+        {
+
+            return ViewState["tableSubjects"] as Hashtable;
+        }
+        set
+        {
+            ViewState["tableSubjects"] = value;
+        }
+    }
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -21,17 +52,26 @@ public partial class Librarian_ManageBooks : System.Web.UI.Page
     }
     protected void BooksMenu_MenuItemClick(object sender, MenuEventArgs e)
     {
+        tableAuthors = new Hashtable();
+        tableSubjects = new Hashtable();
         Menu mi = (Menu)sender;
 
         BooksView.ActiveViewIndex = int.Parse(mi.SelectedValue);
+        GridView1.SelectedIndex = -1;
     }
-    
+
     protected void btnEditSave_Click(object sender, EventArgs e)
     {
         BooksView.ActiveViewIndex = 0;
     }
-    protected void GridView1_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
+    protected void editBook(object sender, GridViewSelectEventArgs e)
     {
+        GridViewRow row = GridView1.Rows[e.NewSelectedIndex];
+        int bookId = int.Parse(row.Cells[1].Text);
+        //set text boxes
+        //set hash tables
+        prepareWizardToEdit(bookId);
+        //button save
         BooksView.ActiveViewIndex = 1;
     }
     protected void btnEditInstanceSave_Click(object sender, EventArgs e)
@@ -61,7 +101,7 @@ public partial class Librarian_ManageBooks : System.Web.UI.Page
     private bool validateTxtNewISBN()
     {
         //return true if isbn lenght grater than 0 and less than 21
-        if (txtNewISBN.Text.Length > 0 && txtNewISBN.Text.Length <=20)
+        if (txtNewISBN.Text.Length > 0 && txtNewISBN.Text.Length <= 20)
         {
             return true;
         }
@@ -74,7 +114,7 @@ public partial class Librarian_ManageBooks : System.Web.UI.Page
     private bool validateTxtTitle()
     {
         //return true if isbn lenght grater than 0 and less than 101
-        if(txtNewTitle.Text.Length > 0 && txtNewTitle.Text.Length <= 100)
+        if (txtNewTitle.Text.Length > 0 && txtNewTitle.Text.Length <= 100)
         {
             return true;
         }
@@ -88,7 +128,8 @@ public partial class Librarian_ManageBooks : System.Web.UI.Page
     private bool validateTxtEdition()
     {
         //return true if edition less than 11
-        if( txtNewEdition.Text.Length < 11){
+        if (txtNewEdition.Text.Length < 11)
+        {
             return true;
         }
         else
@@ -110,20 +151,29 @@ public partial class Librarian_ManageBooks : System.Web.UI.Page
         txtNewSummary.Text = "";
         txtNewTitle.Text = "";
     }
+    private void resetHashtables()
+    {
+        tableAuthors = new Hashtable();
+        tableSubjects = new Hashtable();
+        updateLstNewAuthors();
+        updateLstNewSubjects();
+    }
 
     protected void btnCancel_Click(object sender, EventArgs e)
     {
         resetNewBookFields();
+        resetHashtables();
         resetErrorMessages();
         BooksView.ActiveViewIndex = 0;
+        Wizard1.ActiveStepIndex = 0;
     }
 
     private int saveNewBook(SqlConnection con)
     {
         //sql to insert a new book and retrieve the id created
-        string sqlInsert = "INSERT INTO [Books] ([Isbn], [Title], [Summary], [Edition]) VALUES"+
+        string sqlInsert = "INSERT INTO [Books] ([Isbn], [Title], [Summary], [Edition]) VALUES" +
             "(@Isbn, @Title, @Summary, @Edition);" + "SELECT SCOPE_IDENTITY()";
-        
+
         int id;
 
         SqlCommand cmd = new SqlCommand(sqlInsert, con);
@@ -136,7 +186,7 @@ public partial class Librarian_ManageBooks : System.Web.UI.Page
         cmd.Parameters["@Title"].Value = txtNewTitle.Text;
         cmd.Parameters["@Summary"].Value = txtNewSummary.Text;
         cmd.Parameters["@Edition"].Value = txtNewEdition.Text;
-        
+
         id = int.Parse(cmd.ExecuteScalar().ToString());
 
 
@@ -144,19 +194,223 @@ public partial class Librarian_ManageBooks : System.Web.UI.Page
         //return the id of the created book
         return id;
     }
+
+    private void updateBook(int bookId, SqlConnection con)
+    {
+        string sqlInsert = "UPDATE Books SET Isbn = @Isbn, Title = @Title, Summary = @Summary, Edition = @Edition WHERE BookId = @BookID";
+        SqlCommand cmd = new SqlCommand(sqlInsert, con);
+        cmd.Parameters.AddWithValue("@BookId", bookId);
+        cmd.Parameters.AddWithValue("@Isbn", txtNewISBN.Text);
+        cmd.Parameters.AddWithValue("@Title", txtNewTitle.Text);
+        cmd.Parameters.AddWithValue("@Summary", txtNewSummary.Text);
+        cmd.Parameters.AddWithValue("@Edition", txtNewEdition.Text);
+        cmd.ExecuteNonQuery();
+    }
+
+    private void updateAuthors(int bookId, SqlConnection con)
+    {
+        string sql = "DELETE FROM AuthorsInBooks WHERE BookId = @BookID";
+        SqlCommand cmd = new SqlCommand(sql, con);
+        cmd.Parameters.AddWithValue("@BookId", bookId);
+        cmd.ExecuteNonQuery();
+        saveNewAuthors(bookId, con);
+    }
+    private void updateSubjects(int bookId, SqlConnection con)
+    {
+        string sql = "DELETE FROM SubjectsInBooks WHERE BookId = @BookID";
+        SqlCommand cmd = new SqlCommand(sql, con);
+        cmd.Parameters.AddWithValue("@BookId", bookId);
+        cmd.ExecuteNonQuery();
+        saveNewSubjects(bookId, con);
+    }
+
+    private void saveNewAuthors(int bookId, SqlConnection con)
+    {
+
+        string sql = "INSERT INTO [AuthorsInBooks] ([BookId], [AuthorId]) VALUES (@BookId, @AuthorId)";
+        SqlCommand cmd;
+        foreach (var authorId in tableAuthors.Keys)
+        {
+            cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@BookId", bookId);
+            cmd.Parameters.AddWithValue("@AuthorId", authorId);
+            cmd.ExecuteNonQuery();
+        }
+    }
+
+    private void saveNewSubjects(int bookId, SqlConnection con)
+    {
+
+        string sql = "INSERT INTO [SubjectsInBooks] ([BookId], [SubjectId]) VALUES (@BookId, @SubjectId)";
+        SqlCommand cmd;
+        foreach (var authorId in tableSubjects.Keys)
+        {
+            cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@BookId", bookId);
+            cmd.Parameters.AddWithValue("@SubjectId", authorId);
+            cmd.ExecuteNonQuery();
+        }
+    }
+
     protected void Wizard1_FinishButtonClick(object sender, WizardNavigationEventArgs e)
     {
         SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\aspnet-librarySystem-20150310153417.mdf;Integrated Security=True;Connect Timeout=30;User Instance=False;");
         con.Open();
-        int id = saveNewBook(con);
-        GridView1.DataBind();
+        if (GridView1.SelectedIndex == -1)
+        {
+            int bookId = saveNewBook(con);
 
+            saveNewAuthors(bookId, con);
+            saveNewSubjects(bookId, con);
+        }
+        else
+        {
+            int bookId = int.Parse(GridView1.SelectedDataKey.Value.ToString());
+            updateBook(bookId, con);
+            updateAuthors(bookId, con);
+            updateSubjects(bookId, con);
 
+        }
         //close the connection
         con.Close();
-
-
+        resetNewBookFields();
+        resetHashtables();
+        GridView1.DataBind();
         BooksView.ActiveViewIndex = 0;
+        Wizard1.ActiveStepIndex = 0;
 
     }
+
+
+
+    protected void btnNewAuthorAdd_Click(object sender, EventArgs e)
+    {
+        if (lstNewAllAuthors.SelectedIndex != -1)
+        {
+            if (!tableAuthors.ContainsKey(lstNewAllAuthors.SelectedValue))
+            {
+                tableAuthors.Add(lstNewAllAuthors.SelectedValue, lstNewAllAuthors.SelectedItem.Text);
+                updateLstNewAuthors();
+            }
+
+        }
+
+    }
+
+    private void updateLstNewAuthors()
+    {
+        lstNewAuthors.Items.Clear();
+
+        foreach (DictionaryEntry row in tableAuthors)
+        {
+            lstNewAuthors.Items.Add(new ListItem(row.Value.ToString(), row.Key.ToString()));
+        }
+
+    }
+    protected void btnRemoveNewAuthor_Click(object sender, EventArgs e)
+    {
+        if (lstNewAuthors.SelectedIndex != -1)
+        {
+            tableAuthors.Remove(lstNewAuthors.SelectedValue);
+            updateLstNewAuthors();
+        }
+    }
+    protected void Wizard1_NextButtonClick(object sender, WizardNavigationEventArgs e)
+    {
+        if (!validateFields())
+        {
+            e.Cancel = true;
+        }
+    }
+    protected void btnAddNewSubject_Click(object sender, EventArgs e)
+    {
+        if (lstNewAllSubjects.SelectedIndex != -1)
+        {
+            if (!tableSubjects.ContainsKey(lstNewAllSubjects.SelectedValue))
+            {
+                tableSubjects.Add(lstNewAllSubjects.SelectedValue, lstNewAllSubjects.SelectedItem.Text);
+                updateLstNewSubjects();
+            }
+
+        }
+    }
+
+    private void updateLstNewSubjects()
+    {
+        lstNewSubjects.Items.Clear();
+
+        foreach (DictionaryEntry row in tableSubjects)
+        {
+            lstNewSubjects.Items.Add(new ListItem(row.Value.ToString(), row.Key.ToString()));
+        }
+    }
+    protected void btnRemoveNewSubject_Click(object sender, EventArgs e)
+    {
+        if (lstNewSubjects.SelectedIndex != -1)
+        {
+            tableSubjects.Remove(lstNewSubjects.SelectedValue);
+            updateLstNewSubjects();
+        }
+    }
+
+    private void prepareWizardToEdit(int bookId)
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\aspnet-librarySystem-20150310153417.mdf;Integrated Security=True");
+        con.Open();
+
+        prepareTextBoxesToEdit(bookId, con);
+        prepareTableAuthorToEdit(bookId, con);
+        prepareTableSubjectsToEdit(bookId, con);
+
+        con.Close();
+    }
+    private void prepareTextBoxesToEdit(int bookId, SqlConnection con)
+    {
+        SqlCommand cmd = new SqlCommand("SELECT [BookId],[Isbn],[Title],[Summary], [Edition] FROM Books WHERE [BookId] = @BookId", con);
+        cmd.Parameters.AddWithValue("@BookId", bookId);
+        SqlDataReader dr = cmd.ExecuteReader();
+
+        if (dr.Read())
+        {
+            txtNewISBN.Text = dr["Isbn"].ToString();
+            txtNewTitle.Text = dr["Title"].ToString();
+            txtNewSummary.Text = dr["Summary"].ToString();
+            txtNewEdition.Text = dr["Edition"].ToString();
+
+        }
+
+        dr.Close();
+
+    }
+    private void prepareTableAuthorToEdit(int bookId, SqlConnection con)
+    {
+        tableAuthors = new Hashtable();
+
+        SqlCommand cmd = new SqlCommand("SELECT [a].[AuthorId],[AuthorFirstName],[AuthorSurname] FROM Authors AS [a] INNER JOIN AuthorsInBooks AS [ab] ON [a].[AuthorId] = [ab].[AuthorId] WHERE [ab].[BookId] = @BookId", con);
+        cmd.Parameters.AddWithValue("@BookId", bookId);
+        SqlDataReader dr = cmd.ExecuteReader();
+        while (dr.Read())
+        {
+            tableAuthors.Add(int.Parse(dr["AuthorId"].ToString()), dr["AuthorFirstName"].ToString() + " " + dr["AuthorSurname"].ToString());
+        }
+        dr.Close();
+        updateLstNewAuthors();
+
+    }
+    private void prepareTableSubjectsToEdit(int bookId, SqlConnection con)
+    {
+        tableSubjects = new Hashtable();
+
+        SqlCommand cmd = new SqlCommand("SELECT [s].[SubjectId],[SubjectName] FROM Subjects AS [s] INNER JOIN SubjectsInBooks AS [sb] ON [s].[SubjectId] = [sb].[SubjectId] WHERE [sb].[BookId] = @BookId", con);
+        cmd.Parameters.AddWithValue("@BookId", bookId);
+        SqlDataReader dr = cmd.ExecuteReader();
+        while (dr.Read())
+        {
+            tableSubjects.Add(int.Parse(dr["SubjectId"].ToString()), dr["SubjectName"].ToString());
+        }
+        dr.Close();
+        updateLstNewSubjects();
+    }
+
+
 }
